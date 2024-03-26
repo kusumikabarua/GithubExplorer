6fdbde5c02abcb9416d823487acadb5b426ce350
KB <kb@prashants-MacBook-Pro-2.local>
2024-03-26 14:04:17 +0530

    Add node folder structure

---
+++ src/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config({path :"src/.env"});
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+const cors =require("cors");
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Couldnt connect to MongoDB",err));
+app.listen(PORT, (req, res) => {
+  console.log("Server Listening as port 4000");
+});
+app.get("/", (req, res) => {
+  res.send("Backend server is running");
+});
