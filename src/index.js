c78d1bfc83566b25623f7278710a13580f979d01
KB <kb@prashants-MacBook-Pro-2.local>
2024-03-27 11:25:17 +0530

    Git api removed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 require("dotenv").config({path :"src/.env"});
+const middlewares = require('./middlewares/middlewares');
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
+const routes = require('./routes/routes');
 
 const cors =require("cors");
 app.use(cors());
 app.use(express.json());
+app.use(middlewares.setHeaders);
+app.use('/github_api', routes);
 
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -16,5 +20,5 @@ app.listen(PORT, (req, res) => {
   console.log("Server Listening as port 4000");
 });
 app.get("/", (req, res) => {
-  res.send("Backend server is running");
+  res.send('Welcome to Github NodeJS API app!')
 });
